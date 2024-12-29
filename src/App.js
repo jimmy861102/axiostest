23650025627167f3af9d19886523a3539754995b
jimmy861102 <jimmy861102@gmail.com>
2024-12-29 15:56:19 +0800

    get test

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState(null);
+
+
+  const [employees, setEmployees] = useState(null)
+  const [error, setError] = useState(null)
+
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => setData(response.data))
-      .catch((error) => console.error("请求数据时出错：", error));
+    const getEmployees = async () => {
+      try {
+        const result = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+        setEmployees(result.data);
+      } catch (error) {
+        setError(error);
+        if (error) return `Error: ${error.message}`;
+      }
+    }
+    getEmployees();
   }, []);
+
+console.log(employees);
+
 
   return (
     <div>
       <h1>Axios 示例</h1>
-      {data ? <p>{data.title}</p> : <p>加载中...</p>}
+      <p>{employees.id}</p>
     </div>
   );
 }
